bcb5ef0a982bf46610d1e2c28213fe46073f2a20
Nagarani <51693052+nagaranikarthikeyan@users.noreply.github.com>
2019-07-04 12:46:58 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,25 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int n;
+       Scanner sc=new Scanner(System.in);
+		System.out.print("Input :");
+		try
+		{
+		 n = sc.nextInt();
+		 System.out.println("Output : ");
+		 for(int i=1;i<=n;i++)
+		 {
+			 System.out.println("Hello");
+		 }
+		}
+		catch (InputMismatchException e) {
+			System.out.println("Invalid Input");
+		}
+	}
+
+}
