6b6874d97cbc2f327af34e23575218da0207bc28
Nagarani <51693052+nagaranikarthikeyan@users.noreply.github.com>
2019-07-04 12:41:51 +0530

    Create LeapYear.java

---
+++ LeapYear.java
@@ -0,0 +1,31 @@
+package com.pack;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class LeapYear {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int year;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Input :");
+		try
+		{
+		 year = sc.nextInt();
+		 if(year%4==0)
+		 {
+			 System.out.println("Output : Yes");
+		 }
+		 else
+		 {
+			 System.out.println("Output : No");
+		 }
+		}
+		catch (InputMismatchException e) {
+			System.out.println("Invalid Input");
+		}
+
+	}
+
+}
