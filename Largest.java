5da0168949b56c9591f0170ead3199061c9d20c1
Nagarani <51693052+nagaranikarthikeyan@users.noreply.github.com>
2019-07-04 12:38:01 +0530

    Create Largest.java

---
+++ Largest.java
@@ -0,0 +1,45 @@
+package com.pack;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Largest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				int a,b,c;
+				Scanner sc=new Scanner(System.in);
+				System.out.println("Input :");
+				try
+				{
+				
+				a=sc.nextInt();
+				b=sc.nextInt();
+				c=sc.nextInt();
+				
+				if((a>=b)&&(a>=c))
+				{
+					System.out.println(a);
+				}
+				else if((b>=a)&&(b>=c))
+				{
+					System.out.println(b);
+				}
+				else 
+				{
+					System.out.println(c);
+				}
+				
+				}
+				catch(InputMismatchException ex)
+				{
+					System.out.println("Invalid Input");
+				}
+				
+				
+			
+
+		
+	}
+
+}
