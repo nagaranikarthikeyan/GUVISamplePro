e02d07dc0f8f3665606391ed479f441d369ccf46
Nagarani <51693052+nagaranikarthikeyan@users.noreply.github.com>
2019-07-04 10:38:33 +0530

    Create NumberPrg.java

---
+++ NumberPrg.java
@@ -0,0 +1,39 @@
+package com.pack;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class NumberPrg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int n;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter A number:");
+		try
+		{
+		
+		n=sc.nextInt();
+		if(n<0)
+		{
+			System.out.println("Negative");
+		}
+		else if(n>0)
+		{
+			System.out.println("Positive");
+		}
+		else if(n==0)
+		{
+			System.out.println("Zero");
+		}
+		}
+		catch(InputMismatchException ex)
+		{
+			System.out.println("Invalid Input");
+		}
+		
+		
+	}
+
+}
